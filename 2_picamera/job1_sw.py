ecfe6e047cf7453ff1d503ac0db8f69becbefc87
hirim <53498120+hirim@users.noreply.github.com>
2024-09-24 10:02:18 +0900

    Add files via upload
     take a picture whenever pressing Switch

---
+++ 2_picamera/job1_sw.py
@@ -0,0 +1,12 @@
+from picamera2 import Picamera2, Preview 
+from time import sleep
+
+picam2 = Picamera2() 
+picam2.start_preview(Preview.QTGL) 
+picam2.start() 
+
+sleep(2)
+picam2.capture_file('first.jpg') 
+
+sleep(2) 
+picam2.close()
